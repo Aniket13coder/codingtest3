02a2d6e674294330bc1c90369b95491584eb4fb3
Aniket13coder <mishra.aniket1314@gmail.com>
2026-06-18 20:47:37 +0530

    Add files via upload

---
+++ pythontest3.py
@@ -0,0 +1,4 @@
+students= {"Name": "Aniket","Score": 56,"Name": "Abraham","Score": 678, "Name": "Adhi","Score": 87,"Name": "Rohith", "Score": 45,"Name": "Virat","Score": 344}
+for keys,values in students.
+    t
+    
